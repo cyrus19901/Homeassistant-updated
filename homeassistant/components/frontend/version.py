9f8098f4d3d4ce95543a1b492d708464ed5ebb22
Raj <cyrus19901@gmail.com>
2019-01-16 14:49:19 -0800

    adding all the fixes

--- homeassistant/components/frontend/version.py
+++ homeassistant/components/frontend/version.py
@@ -3,10 +3,10 @@
 FINGERPRINTS = {
     "compatibility.js": "1686167ff210e001f063f5c606b2e74b",
     "core.js": "2a7d01e45187c7d4635da05065b5e54e",
-    "frontend.html": "ff4edf839c6b9ae05030c5fc7c289ddb",
+    "frontend.html": "9ebc6b538ee803e12284ffe511f4c173",
     "mdi.html": "2e848b4da029bf73d426d5ba058a088d",
     "micromarkdown-js.html": "93b5ec4016f0bba585521cf4d18dec1a",
-    "panels/ha-panel-config.html": "d1b4d726d75a05461098a3d9afae7800",
+    "panels/ha-panel-config.html": "fa87e24ece2043dc01f0ae135a6b026e",
     "panels/ha-panel-dev-event.html": "d409e7ab537d9fe629126d122345279c",
     "panels/ha-panel-dev-info.html": "b0e55eb657fd75f21aba2426ac0cedc0",
     "panels/ha-panel-dev-mqtt.html": "94b222b013a98583842de3e72d5888c6",
@@ -19,6 +19,6 @@ FINGERPRINTS = {
     "panels/ha-panel-kiosk.html": "b40aa5cb52dd7675bea744afcf9eebf8",
     "panels/ha-panel-logbook.html": "771afdcf48dc7e308b0282417d2e02d8",
     "panels/ha-panel-mailbox.html": "a8cca44ca36553e91565e3c894ea6323",
-    "panels/ha-panel-map.html": "565db019147162080c21af962afc097f",
+    "panels/ha-panel-map.html": "ba4e74fce8376d4e1feab525ecd56400",
     "panels/ha-panel-shopping-list.html": "d8cfd0ecdb3aa6214c0f6908c34c7141"
 }
